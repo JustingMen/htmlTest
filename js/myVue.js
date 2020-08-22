ac51874a232475981c246dee459b828a95eb5b62
472990701@qq.com <6571064+zhouningJusty@user.noreply.gitee.com>
2020-08-22 15:16:31 +0800

    添加了虚拟DOM碎片方法

--- js/myVue.js
+++ js/myVue.js
@@ -1,9 +1,25 @@
 class Compiler {
-  constructor( node, vm ) {
-    this.el = node.nodeType === 1 ? el : document.querySelector( el );
+  constructor( el, vm ) {
+    this.el = el.nodeType === 1 ? el : document.querySelector( el );
     this.vm = vm;
 
+    // 将模板 DOM 保存在 虚拟 DOM 碎片中
     let fragment = this.node2Fragment( this.el );
+    console.log( fragment );
+  }
+
+  node2Fragment ( node ) {
+    
+    // 创建一个虚拟 DOM 碎片，用来存放 模板DOM 节点
+    let fragment = document.createDocumentFragment();
+    let firstChild = null;
+
+    //将模板 DOM 中的所有元素放入虚拟 DOM 碎片中
+    while( firstChild = node.firstChild ) {
+      fragment.appendChild( firstChild );
+    }
+
+    return fragment;
   }
 }
 
